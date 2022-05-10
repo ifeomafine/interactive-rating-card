229cce655db634162ad27bf35efaa62bb55888fb
ifeomafine <102186386+ifeomafine@users.noreply.github.com>
2022-05-10 20:11:43 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+
+
+/*const form = document.querySelector('.buttons');
+const radioInputs = document.querySelectorAll('.btn');
+const ratingCard = document.querySelector('.front')
+
+let ratingValue;
+
+radioInputs.forEach(input => {
+    input.addEventListener('change' , (e) => {
+        ratingValue = e.target.value;
+    })
+})
+form.addEventListener('submit' , e => {
+    e.preventDefault();
+    if(!ratingValue) return;
+    renderThankyou(ratingCard, ratingValue)
+})
+function renderThankyou (container, _ratingValue) {
+    container.innerHTML = 
+    <div id="back">
+    <div class="icon">
+      <img src="images/illustration-thank-you.svg" alt="Icon">
+      <br>  <br>
+    </div>
+    <h6>You selected $(ratingValue) out of 5 </h6>
+    <br> <br>
+    <h2>Thank you!</h2>
+    <br>
+    <div>
+  <p> We appreciate you taking the time to give a rating. If you ever need more support, 
+    don't hesitate to get in touch!</p> 
+    </div>'
+} */
