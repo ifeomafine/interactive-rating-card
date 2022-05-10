701c748d127810878640487bc3a2e24c75da9794
ifeomafine <102186386+ifeomafine@users.noreply.github.com>
2022-05-11 00:20:37 +0100

    updated codes

--- script.js
+++ script.js
@@ -1,34 +1,21 @@
+var rateNumbers = document.querySelectorAll(".rate__numbers li")
+var choice = document.getElementById("choice");
+var thank = document.querySelector(".thank");
 
 
-/*const form = document.querySelector('.buttons');
-const radioInputs = document.querySelectorAll('.btn');
-const ratingCard = document.querySelector('.front')
-
-let ratingValue;
-
-radioInputs.forEach(input => {
-    input.addEventListener('change' , (e) => {
-        ratingValue = e.target.value;
+rateNumbers.forEach((node) => {
+    node.addEventListener("click", function(){
+        var result =  node.innerText;
+        choice.innerHTML = result;
     })
+});
+
+thank.addEventListener("click", function(){
+    document.querySelector(".front").style.display ="none";
+    setInterval(() => {
+        location.reload();
+    }, 5000);
 })
-form.addEventListener('submit' , e => {
-    e.preventDefault();
-    if(!ratingValue) return;
-    renderThankyou(ratingCard, ratingValue)
-})
-function renderThankyou (container, _ratingValue) {
-    container.innerHTML = 
-    <div id="back">
-    <div class="icon">
-      <img src="images/illustration-thank-you.svg" alt="Icon">
-      <br>  <br>
-    </div>
-    <h6>You selected $(ratingValue) out of 5 </h6>
-    <br> <br>
-    <h2>Thank you!</h2>
-    <br>
-    <div>
-  <p> We appreciate you taking the time to give a rating. If you ever need more support, 
-    don't hesitate to get in touch!</p> 
-    </div>'
-} */
+// function submit(){
+//    document.querySelector(".front").style.display ="none";
+// }
